6a422695591760e433ebc8308721ba192cf1393a
xpat <patrycja.kruczynska@gmail.com>
2025-04-24 16:43:32 +0200

    fix fare calculator with getTime function

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -10,11 +10,9 @@ public class FareCalculatorService {
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
-
-        //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+        long inHour = ticket.getInTime().getTime();
+        long outHour = ticket.getOutTime().getTime();
+        double duration = (outHour - inHour) / (1000.0 * 60 * 60);
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
