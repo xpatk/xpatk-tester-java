1837fe237f31eb9a6cb311b7dadc4cff48a0ed89
xpat <patrycja.kruczynska@gmail.com>
2025-04-25 12:15:27 +0200

    added discount to ticket price calculation

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -5,7 +5,7 @@ import com.parkit.parkingsystem.model.Ticket;
 
 public class FareCalculatorService {
 
-    public void calculateFare(Ticket ticket){
+    public void calculateFare(Ticket ticket, boolean discount){
         if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
@@ -23,11 +23,13 @@ public class FareCalculatorService {
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+                double price = duration * Fare.CAR_RATE_PER_HOUR;
+                ticket.setPrice(discount? price * 0.95 : price);
                 break;
             }
             case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+                double price = duration * Fare.BIKE_RATE_PER_HOUR;
+                ticket.setPrice(discount? price * 0.95 : price);
                 break;
             }
             default: throw new IllegalArgumentException("Unknown Parking Type");
