cafc6810e960ee764bfede739af4d84a78377c84
xpat <patrycja.kruczynska@gmail.com>
2025-06-26 01:41:06 +0200

    timezone fix

--- src/main/java/com/parkit/parkingsystem/service/ParkingService.java
+++ src/main/java/com/parkit/parkingsystem/service/ParkingService.java
@@ -32,6 +32,10 @@ public class ParkingService {
             ParkingSpot parkingSpot = getNextParkingNumberIfAvailable();
             if(parkingSpot !=null && parkingSpot.getId() > 0){
                 String vehicleRegNumber = getVehicleRegNumber();
+                int nbTickets = ticketDAO.getNbTickets(vehicleRegNumber);
+                if (nbTickets > 0) {
+                    System.out.println("Welcome back! As a recurring user of our parking lot, you'll benefit from a 5% discount.");
+                }
                 parkingSpot.setAvailable(false);
                 parkingSpotDAO.updateParking(parkingSpot);//allot this parking space and mark its availability as false
 
